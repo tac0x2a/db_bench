72d14dec6cdeb9261c327dedaea8a0871dd47d1e
TAC <tac@tac42.net>
2018-09-08 23:04:25 +0900

    Upgrade generate script

--- generate_testdata.py
+++ generate_testdata.py
@@ -1,24 +1,35 @@
-
-ROW_COUNT = 1000000
-CSV_FILE = "bench.csv"
-
-# --
-import csv
-
+#!/usr/bin/env python
 
 def write_rows(writer, count):
+    import random as r
+
     from faker import Faker # pip install faker
     fake = Faker('jp_JP')
     fake.seed(4321)
 
     # Header
-    writer.writerow(['uid', 'name', 'code', 'address', 'color', 'create_at'])
+    writer.writerow(['uid', 'name', 'code', 'address', 'color', 'random','create_at'])
 
-    for i in range(1, ROW_COUNT + 1):
-        writer.writerow([i, fake.name(), fake.ean13(), fake.address(), fake.color_name(), fake.date_time_this_year()])
+    from progressbar import ProgressBar # pip install progressbar2
+    p = ProgressBar(1, count)
 
-with open(CSV_FILE, 'w') as f:
+    for i in range(1, count + 1):
+        writer.writerow([i, fake.name(), fake.ean13(), fake.address(), fake.color_name(), r.randint(0, int(count/2)), fake.date_time_this_year()])
+        p.update(i)
+
+# Entry Point
+import argparse
+parser = argparse.ArgumentParser(description='Generate csv file that filled fake data')
+parser.add_argument('row_count', metavar='N', type=int, help='Number of rows')
+parser.add_argument('-o', help='Destination of generated file', default='bench.csv')
+args = parser.parse_args()
+
+row_count = vars(args)['row_count']
+csv_file  = vars(args)['o']
+print('row_count:{}'.format(row_count))
+print('destination:{}'.format(csv_file))
+
+with open(csv_file, 'w') as f:
+    import csv
     writer = csv.writer(f)
-    write_rows(writer, ROW_COUNT)
-
-#
+    write_rows(writer, row_count)
